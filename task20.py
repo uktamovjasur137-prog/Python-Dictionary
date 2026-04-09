eb1cc47e62456c7c19d9ce4f75abeae4c3c660b0
Jasur <uktamovjasur137@gmail.com>
2026-04-09 03:25:34 -0700

    done

--- task20.py
+++ task20.py
@@ -1,17 +1,8 @@
-{
-        "id": 1,
-        "name": "s22",
-        "price": 450,
-        "quantity": 3,
-    },
-    {
-        "id": 2,
-        "name": "s23",
-        "price": 550,
-        "quantity": 2,
-    },
-    {
-        "id": 3,
-        "name": "s24",
-        "price": 650,
-        "quantity": 8,
+permissions = {
+    "read": True,
+    "write": False,
+    "delete": True
+}
+for key, value in permissions.items():
+    if value == True:
+        print(key)
