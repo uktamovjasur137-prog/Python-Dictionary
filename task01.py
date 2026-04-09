2d9bb65214df6a79a53a510ab4711c1698afbd44
Jasur <uktamovjasur137@gmail.com>
2026-04-09 02:17:57 -0700

    done

--- task01.py
+++ task01.py
@@ -1,21 +1,7 @@
-products = [
-    {
-        "id": 1,
-        "name": "s22",
-        "price": 450,
-        "quantity": 3,
-    },
-    {
-        "id": 2,
-        "name": "s23",
-        "price": 550,
-        "quantity": 2,
-    },
-    {
-        "id": 3,
-        "name": "s24",
-        "price": 650,
-        "quantity": 8,
-    }
-]
+student = {}
 
+student['name'] = 'Jasur'
+student['age'] = 25
+student['grade'] = 'A'
+
+print(student)
