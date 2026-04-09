5c99566f69a3ee37c5a0599e63e679b2fc66395e
Jasur <uktamovjasur137@gmail.com>
2026-04-08 18:37:24 -0700

    done

---
+++ task20.py
@@ -0,0 +1,17 @@
+{
+        "id": 1,
+        "name": "s22",
+        "price": 450,
+        "quantity": 3,
+    },
+    {
+        "id": 2,
+        "name": "s23",
+        "price": 550,
+        "quantity": 2,
+    },
+    {
+        "id": 3,
+        "name": "s24",
+        "price": 650,
+        "quantity": 8,
