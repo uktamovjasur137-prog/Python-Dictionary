b0e2dba9d69d886d31e9f13e565c2b9631f30ac6
Jasur <uktamovjasur137@gmail.com>
2026-04-09 02:26:45 -0700

    done

---
+++ task03.py
@@ -0,0 +1,12 @@
+user1 = {
+    "name": "Jasur",
+    "email": "jasuruktamov@gmail.com"
+}
+
+user2 = {
+    "name": "Xasan",
+    "email": "xasan188@gmail.com"
+}
+
+users = [user1, user2]
+print(users)
